a5e5e16dea9016f0537a84cb8e944c380c0a9b19
URVASHI-PAHWA <urvashi0231.cse19@chitkara.edu.in>
2022-06-21 16:20:03 +0530

    first commit

---
+++ src/app/personal-loan/personal-loan.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-personal-loan',
+  templateUrl: './personal-loan.component.html',
+  styleUrls: ['./personal-loan.component.css']
+})
+export class PersonalLoanComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+  data:any;
+  sendData(){
+    this.data = (<HTMLInputElement>document.getElementById("amount")).value;
+    this.router.navigate(['/Result'],{state:{data:this.data}});
+  }
+
+}
---
+++ src/app/result/result.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.css']
+})
+export class ResultComponent implements OnInit {
+
+  constructor(private router:Router) { }
+  onBack():void{
+    this.router.navigate(['Result']);
+  }
+  data:any;
+  ngOnInit(): void {
+    this.data = 13*(history.state.data);
+  }
+
+}
